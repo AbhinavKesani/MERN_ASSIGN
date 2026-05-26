4364c3aa796401e82d2ca817704e87d9485bd396
AbhinavKesani <23eg105g29@anurag.edu.in>
2026-05-26 14:28:31 +0530

    Rebuilt AddUser component cleanly

--- week8/Week-8-Assignment/USER-MANAGEMENT/frontend/src/components/AddUser.jsx
+++ week8/Week-8-Assignment/USER-MANAGEMENT/frontend/src/components/AddUser.jsx
@@ -54,6 +54,7 @@ function AddUser() {
   }
 
   return (
+
     <div className='flex items-center justify-center min-h-screen px-4'>
 
       <div className='glass hover-card w-full max-w-md p-10 rounded-3xl'>
@@ -106,6 +107,7 @@ function AddUser() {
       </div>
 
     </div>
+
   )
 }
 
